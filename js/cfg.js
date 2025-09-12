b14bf70d8f8ef16f5e446987ee5cb6649658120b
Fog <blue_fog@163.com>
2025-09-13 07:49:28 +0800

    init

---
+++ js/cfg.js
@@ -0,0 +1,119 @@
+var itemList = [
+    {
+        "title": "搜索",
+        "web": [
+            { "name": "Bing", "url": "https://cn.bing.com" },
+            { "name": "Google", "url": "https://www.google.com.hk/" },
+            { "name": "百度", "url": "https://www.baidu.com/" },
+            { "name": "百度 Chat", "url": "https://chat.baidu.com/" }
+        ]
+    },
+    {
+        "title": "数据仓库",
+        "web": [
+            { "name": "Github", "url": "https://www.github.com" },
+            { "name": "Bitbucket", "url": "https://bitbucket.org/" },
+            { "name": "开源中国", "url": "https://git.oschina.net/" }
+        ]
+    },
+    {
+        "title": "AI",
+        "web": [
+            { "name": "魔塔社区", "url": "https://www.modelscope.cn/aigc/home" },
+            { "name": "DeepSeek", "url": "https://chat.deepseek.com" },
+        ]
+    },
+    {
+        "title": "Unity3D 相关",
+        "web": [
+            { "name": "Unity3D官网", "url": "https://unity.com/" },
+            { "name": "Unity3D-assetstore", "url": "https://assetstore.unity.com/" },
+            { "name": "Unity 社区", "url": "https://developer.unity.cn/" },
+            { "name": "Unity3D-China", "url": "http://www.u3dchina.com/" }
+        ]
+    },
+    {
+        "title": "社区",
+        "web": [
+            { "name": "163邮箱", "url": "http://mail.163.com" },
+            { "name": "CSDN", "url": "http://www.csdn.net/" },
+            { "name": "Stackoverflow", "url": "https://stackoverflow.com" },
+            { "name": "Segmentfault", "url": "https://segmentfault.com/" },
+            { "name": "Quora", "url": "https://www.quora.com/" },
+            { "name": "掘金", "url": "https://juejin.im/" },
+            { "name": "InfoQ", "url": "https://www.infoq.cn" }
+        ]
+    },
+    {
+        "title": "编程文档",
+        "web": [
+            { "name": ".Net 文档", "url": "https://learn.microsoft.com/zh-cn/dotnet/" },
+            { "name": "c# 教程", "url": "https://learn.microsoft.com/zh-cn/dotnet/csharp/tour-of-csharp/" },
+            { "name": "cheat.sh", "url": "https://cheat.sh" },
+            { "name": "Python 语法参考", "url": "https://www.runoob.com/python3/python3-tutorial.html" },
+            { "name": "Unity Cg Programming", "url": "http://en.wikibooks.org/wiki/Cg_Programming/Unity" },
+            { "name": "Haskell 手册", "url": "https://www.haskell.org/hoogle" },
+            { "name": "Lua 语法参考", "url": "http://www.runoob.com/lua/lua-tutorial.html" },
+            { "name": "Redis 教程", "url": "http://www.runoob.com/redis/redis-tutorial.html" },
+            { "name": "Linux 命令大全", "url": "https://www.runoob.com/linux/linux-command-manual.html" }
+        ]
+    },
+    {
+        "title": "娱乐",
+        "web": [
+            { "name": "hao123", "url": "http://www.hao123.com/" },
+            { "name": "电影天堂", "url": "https://www.dy2018.com/" },
+            { "name": "爱奇艺", "url": "http://www.iqiyi.com/" },
+            { "name": "YouTube", "url": "https://www.youtube.com" },
+            { "name": "腾讯视频", "url": "https://v.qq.com/" },
+            { "name": "哔哩哔哩", "url": "https://www.bilibili.com/" },
+            { "name": "CCTV5", "url": "https://tv.cctv.com/cctv5/" }
+        ]
+    },
+    {
+        "title": "工具",
+        "web": [
+            { "name": "欧陆词典", "url": "https://dict.eudic.net/" },
+            { "name": "Google云端硬盘", "url": "https://drive.google.com" },
+            { "name": "Git参考", "url": "http://git-scm.com/" },
+            { "name": "Json解析", "url": "http://json.cn" },
+            { "name": "站长工具", "url": "http://tool.chinaz.com/Tools/unixtime.aspx" },
+            { "name": "印象笔记", "url": "https://www.yinxiang.com/" },
+            { "name": "网易云阅读", "url": "http://yuedu.163.com/" }
+        ]
+    },
+    {
+        "title": "工作",
+        "web": [
+            { "name": "phpAdmin 本地", "url": "http://127.0.0.1/phpmyadmin" },
+            { "name": "bugly", "url": "https://bugly.qq.com/v2/index" }
+        ]
+    },
+    {
+        "title": "其他",
+        "web": [
+            { "name": "OpenGameArt", "url": "http://www.opengameart.org/" },
+            { "name": "EasyIcon", "url": "http://www.easyicon.net/" },
+            { "name": "爱给素材", "url": "https://www.aigei.com/" },
+            { "name": "天气预报", "url": "https://wttr.in/" },
+            { "name": "Giphy[GIF]", "url": "https://giphy.com/apps/giphy" }
+        ]
+    },
+    {
+        "title": "购物",
+        "web": [
+            { "name": "淘宝", "url": "https://www.taobao.com/" },
+            { "name": "京东", "url": "http://www.jd.com/" },
+            { "name": "当当", "url": "http://www.dangdang.com/" }
+        ]
+    },
+    {
+        "title": "PHP 相关",
+        "web": [
+            { "name": "Php 语法参考", "url": "http://www.w3cschool.cc/php/php-tutorial.html" },
+            { "name": "GatewayWorker 手册", "url": "http://doc2.workerman.net" },
+            { "name": "swoole 官网", "url": "https://www.swoole.com" },
+            { "name": "swoole 文档", "url": "https://wiki.swoole.com" }
+        ]
+    }
+];
